fc835e23f7e14a8ff5728c9c3cf997a45195a8c4
Timo <tifauss@gmail.com>
2023-11-01 13:55:29 -0400

    HW2 - initial

---
+++ HW2/src/animals/Bird.java
@@ -0,0 +1,24 @@
+package animals;
+
+public class Bird implements Petable{
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int getWeightInOz() {
+        return 0;
+    }
+
+    @Override
+    public int getAge() {
+        return 0;
+    }
+
+    @Override
+    public int eats(String foodLabel) {
+        return 0;
+    }
+}
