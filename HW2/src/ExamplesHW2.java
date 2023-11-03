2cea2f7717d53d173959b5d35f9ef2d4ef767f2c
yyi-wpi <143128263+yyi-wpi@users.noreply.github.com>
2023-11-03 15:09:47 -0400

    update..

--- HW2/src/ExamplesHW2.java
+++ HW2/src/ExamplesHW2.java
@@ -7,9 +7,9 @@ import java.util.LinkedList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-public class Examples {
+public class ExamplesHW2 {
 
-    public Examples(){
+    public ExamplesHW2(){
 
     }
 
