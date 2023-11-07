f3387a4910b188f017201470dd3464a28e105ff0
yyi-wpi <143128263+yyi-wpi@users.noreply.github.com>
2023-11-07 15:29:51 -0500

    made change on GreenHouseProduce.java

--- HW3/src/Examples.java
+++ HW3/src/Examples.java
@@ -11,6 +11,7 @@ public class Examples {
         values = List.of(20231106010101.0,45.5,34.0,46.6,40.0,20231130020202.0,22.2,20.0,35.5,30.0,-999.0,31.0,32.2,-999.0);
     }
 
+
     @Test
     public void testSomething(){
         assertEquals(1,1, 0.01);
--- HW3/src/GreenHouseProduce.java
+++ HW3/src/GreenHouseProduce.java
@@ -1,24 +1,49 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class GreenHouseProduce extends AbsGreenHouse implements Sensible{
+public class GreenHouseProduce extends AbsGreenHouse implements Sensible {
 
-    public GreenHouseProduce(){
+    private List<TempHumidReading> readings;
+    private TempHumidReading middleReading;
 
+    public GreenHouseProduce() {
+        // Initialize the readings list
+        readings = new ArrayList<>();
     }
 
     @Override
     public void pollSensorData(List<Double> values) {
+        // Perform the computation to parse and clean the data
+        readings.clear(); // Clear previous readings
+        List<Double> cleanedData = values.stream()
+                .filter(value -> value != -999.0)
+                .collect(Collectors.toList());
 
+        // Assuming the cleanedData list is in the format: date, temp, humidity, temp, humidity, ...
+        for (int i = 0; i < cleanedData.size(); i += 3) {
+            double temperature = cleanedData.get(i + 1);
+            double humidity = cleanedData.get(i + 2);
+            readings.add(new TempHumidReading(temperature, humidity));
+        }
+
+        // Compute and store the middle reading for fast retrieval
+        if (!readings.isEmpty()) {
+            int middleIndex = readings.size() / 2;
+            middleReading = readings.get(middleIndex);
+        }
     }
 
     @Override
     public TempHumidReading middleReading() {
-        return null;
+        // Return the precomputed middle reading
+        return middleReading;
     }
 
     @Override
     public TempHumidReading middleReading(double onDate) {
-
+        // This method would require additional logic to find the reading for a specific date
+        // For now, it's not implemented as it requires more context on how the data is structured
         return null;
     }
 }
