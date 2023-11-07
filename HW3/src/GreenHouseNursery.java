d098de7abe4cd1f1a1ba879f8cb54152b1c4e39b
yyi-wpi <143128263+yyi-wpi@users.noreply.github.com>
2023-11-07 15:22:07 -0500

    edited the file base on your work

--- HW3/src/GreenHouseNursery.java
+++ HW3/src/GreenHouseNursery.java
@@ -1,26 +1,44 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class GreenHouseNursery extends AbsGreenHouse implements Sensible{
+public class GreenHouseNursery extends AbsGreenHouse implements Sensible {
 
-    public GreenHouseNursery(){
+    private List<Double> sensorData;
 
+    public GreenHouseNursery() {
+        // Initialize the sensor data list
+        sensorData = new ArrayList<>();
     }
 
     @Override
     public void pollSensorData(List<Double> values) {
-        //TODO
-        //pollSensorData() should be fast
+        // Quickly store the data, filtering out the error values (-999.0)
+        sensorData = values.stream()
+                .filter(value -> value != -999.0)
+                .collect(Collectors.toList());
     }
 
     @Override
     public TempHumidReading middleReading() {
-        //TODO
-        //middleReading() can perform most of the computation
-        return null;
+        // Perform the computation to find the middle reading
+        // Assuming that the sensorData list is already sorted by date and time
+        if (sensorData.isEmpty()) {
+            return null; // or throw an exception if appropriate
+        }
+
+        // Find the middle index for temperature and humidity
+        int middleIndex = sensorData.size() / 4; // Each reading has 2 values (temp and humidity), so we divide by 4
+        double middleTemperature = sensorData.get(middleIndex * 2);
+        double middleHumidity = sensorData.get(middleIndex * 2 + 1);
+
+        return new TempHumidReading(middleTemperature, middleHumidity);
     }
 
     @Override
     public TempHumidReading middleReading(double onDate) {
+        // Find the middle reading for a specific date
+        // This method will require parsing the date and finding the corresponding readings
+        // For now, it's not implemented as it requires more context on how the data is structured
         return null;
     }
 }
