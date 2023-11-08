3eaaaaf080f2974f46828d2c6a9833c0d6a29794
yyi-wpi <143128263+yyi-wpi@users.noreply.github.com>
2023-11-07 22:47:37 -0500

    did more example tests

--- HW3/src/Examples.java
+++ HW3/src/Examples.java
@@ -1,10 +1,11 @@
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 
 public class Examples {
 
@@ -78,6 +79,89 @@ public class Examples {
 
         assertEquals(true, sthr.equals(sthr1));
     }
+
+    @Test
+    public void testNotEquals() {
+        SuperTempHumidReading sthr =  new SuperTempHumidReading(12.2,13.0);
+        SuperTempHumidReading sthr1 =  new SuperTempHumidReading(12.2,13.3);
+
+        assertEquals(false, sthr.equals(sthr1));
+    }
+
+    @Test
+    public void testDefaultReading() {
+        SuperTempHumidReading sthr =  new SuperTempHumidReading();
+
+        assertEquals("{Err;Err}", sthr.toString());
+    }
+
+    @Test
+    public void testMediumReadingsNursery() {
+        GreenHouseNursery green = new GreenHouseNursery();
+        green.pollSensorData(List.of(20231106010101.0,45.5,34.0,46.6,40.0,20231130020202.0,22.2,20.0,35.5,30.0,-999.0,31.0,32.2,-999.0));
+        assertEquals(new SuperTempHumidReading(35.5,31.0), green.middleReading());
+    }
+
+
+
+    public class GreenHouseProduceTest {
+
+        private GreenHouseProduce greenHouseProduce;
+        private List<Double> sensorData;
+
+        @Before
+        public void setUp() {
+            greenHouseProduce = new GreenHouseProduce();
+            sensorData = List.of(
+                    20231106010101.0, 45.5, 34.0, 46.6, 40.0,
+                    20231130020202.0, 22.2, 20.0, 35.5, 30.0,
+                    -999.0, 31.0, 32.2, -999.0
+            );
+            greenHouseProduce.pollSensorData(sensorData);
+        }
+
+        @Test
+        public void testMiddleReadingProduce() {
+            // Assuming that the middle reading has already been calculated in pollSensorData
+            // and that middleReading() will be a fast retrieval
+            long startTime = System.currentTimeMillis();
+            TempHumidReading middleReading = greenHouseProduce.middleReading();
+            long endTime = System.currentTimeMillis();
+
+            // Check that the middle reading is correct
+            assertEquals(new SuperTempHumidReading(35.5, 31.0), middleReading);
+
+            // Check that the retrieval was fast
+            assertTrue("middleReading should be fast", endTime - startTime < 10);
+        }
+
+        @Test
+        public void testPollSensorDataPerformance() {
+            // This test is to ensure that pollSensorData is doing the heavy lifting
+            // and that it takes longer than middleReading, which should be a simple lookup
+            GreenHouseProduce greenHouseProduceForTest = new GreenHouseProduce();
+
+            long startTimePoll = System.currentTimeMillis();
+            greenHouseProduceForTest.pollSensorData(sensorData);
+            long endTimePoll = System.currentTimeMillis();
+
+            long startTimeMiddle = System.currentTimeMillis();
+            greenHouseProduceForTest.middleReading();
+            long endTimeMiddle = System.currentTimeMillis();
+
+            long pollDuration = endTimePoll - startTimePoll;
+            long middleDuration = endTimeMiddle - startTimeMiddle;
+
+            System.out.println(String.format("pollSensorData() : middleReading() :: %s : %s", pollDuration, middleDuration));
+
+            // Assert that pollSensorData takes more time than middleReading
+            assertTrue("pollSensorData should do most of the computation and take longer", pollDuration > middleDuration);
+        }
+
+        // can add more tests for other methods like middleReading(double onDate) here
+    }
+
+
     //assertEquals(new SuperTempHumidReading(44.4,33.54), myGreenHouse.middleReading());
 
 /*
