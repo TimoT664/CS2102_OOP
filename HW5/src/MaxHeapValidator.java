42345108eb6aee6c23d05623f3984b0d0b8141c1
Timo <tifauss@gmail.com>
2023-12-04 16:42:57 -0500

    HW5 - few comments

--- HW5/src/MaxHeapValidator.java
+++ HW5/src/MaxHeapValidator.java
@@ -1,5 +1,6 @@
 public class MaxHeapValidator implements BTValidator {
 
+    // Checks if adding 'elt' to 'after' from 'before' maintains the properties of a max heap
     @Override
     public boolean validAdd(IBinTree before, int elt, IBinTree after) {
         // Check if before tree does not contain elt, after tree does, and all elements in before are in after
@@ -7,6 +8,7 @@ public class MaxHeapValidator implements BTValidator {
                 containsOnlyElements(before, after, elt) && isValidMaxHeap(after);
     }
 
+    // Checks if removing 'elt' from 'before' to get 'after' maintains the properties of a max heap
     @Override
     public boolean validRemove(IBinTree before, int elt, IBinTree after) {
         // Check if before tree contains elt, after tree does not, and all elements in after are in before
@@ -14,6 +16,7 @@ public class MaxHeapValidator implements BTValidator {
                 containsOnlyElements(after, before, elt) && isValidMaxHeap(after);
     }
 
+    // Checks if 'tree' contains the element 'elt'
     protected boolean containsElt(IBinTree tree, int elt) {
         if (tree instanceof EmptyBT) {
             return false;
@@ -24,6 +27,7 @@ public class MaxHeapValidator implements BTValidator {
         return false;
     }
 
+    // Checks if all elements in 'source' are present in 'target'
     protected boolean containsAll(IBinTree source, IBinTree target) {
         if (source instanceof EmptyBT) {
             return true;
@@ -35,10 +39,12 @@ public class MaxHeapValidator implements BTValidator {
         return false;
     }
 
+    // Checks if 'target' contains only the elements from 'source' and an additional 'additionalElt'
     protected boolean containsOnlyElements(IBinTree source, IBinTree target, int additionalElt) {
         return containsAll(target, new NodeBT(additionalElt, source, new EmptyBT()));
     }
 
+    // Checks if 'tree' is a valid max heap
     protected boolean isValidMaxHeap(IBinTree tree) {
         if (tree instanceof EmptyBT) {
             return true;
