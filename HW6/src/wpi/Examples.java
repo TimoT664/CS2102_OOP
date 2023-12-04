54307a0265108275037ae9b75415535ac0d16ca9
Timo <tifauss@gmail.com>
2023-12-04 13:48:25 -0500

    HW6 - initial commit

---
+++ HW6/src/wpi/Examples.java
@@ -0,0 +1,17 @@
+package wpi;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+public class Examples {
+
+   @Test
+   public void validateAddExistingTree() {
+        assertEquals(1,1,0.1);
+   }
+
+}
